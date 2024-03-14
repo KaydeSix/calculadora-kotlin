8b501c6ea0659b73641926d5c45c3637c667f4bd
KaydeSix <76545267+KaydeSix@users.noreply.github.com>
2024-03-14 19:07:02 -0300

    Alterada a lógica da calculadora

--- calculadora-master/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ calculadora-master/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.calculadora.R
 class MainActivity : AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var resultTextView: TextView
+    private val number2 = 2.0 // é possivel alterar o valor ou pega-lo de outro editText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,24 +20,26 @@ class MainActivity : AppCompatActivity() {
 
     fun performOperation(view: View) {
         val number1 = editText.text.toString().toDoubleOrNull()
-        val number2 = 5.0 // you can replace this with another value or retrieve it from another EditText
+        val result = when (view.id) {
+            R.id.btnAdd -> number1?.plus(number2)
+            R.id.btnSubtract -> number1?.minus(number2)
+            R.id.btnMultiply -> number1?.times(number2)
+            R.id.btnDivide -> number1?.div(number2)
+            else -> null
+        }
 
-        when (view.id) {
-            R.id.btnAdd -> displayResult(number1?.plus(number2))
-            R.id.btnSubtract -> displayResult(number1?.minus(number2))
-            R.id.btnMultiply -> displayResult(number1?.times(number2))
-            R.id.btnDivide -> {
-                if (number2 != 0.0) {
-                    displayResult(number1?.div(number2))
-                } else {
-                    displayResult(null)
-                    resultTextView.text = "Cannot divide by zero"
-                }
-            }
+        if (view.id == R.id.btnDivide && number2 == 0.0) {
+            displayResult(null, "Não é possível dividir por zero")
+        } else {
+            displayResult(result, null)
         }
     }
 
-    private fun displayResult(result: Double?) {
-        resultTextView.text = "Result: ${result ?: "Invalid input"}"
+    private fun displayResult(result: Double?, error: String?) {
+        if (error != null) {
+            resultTextView.text = error
+        } else {
+            resultTextView.text = "Resultado: ${result ?: "Entrada inválida"}"
+        }
     }
 }
